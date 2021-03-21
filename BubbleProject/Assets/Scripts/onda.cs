7c4bd9b7219d3aa9a795207d67135cbf1412ae7d
Marina <aniram.520@gmail.com>
2021-03-21 11:31:16 -0300

    alterações

---
+++ BubbleProject/Assets/Scripts/onda.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* isso aqui vai criar o GameObject associado ao clicar com o lado esquerdo 
+ *  do mouse*/
+
+public class onda : MonoBehaviour
+{
+
+    public GameObject objeto;
+    // Start is called before the first frame update
+    public void Spanw(Vector3 position)
+    {
+        Instantiate(objeto).transform.position = position;
+        // cria o objeto que faz a 'onda'
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Vector3 ponto = Camera.main.ScreenToWorldPoint(Input.mousePosition, Camera.MonoOrStereoscopicEye.Mono);
+            // segundo a Unity doc : Transforma um ponto do espaço da tela em espaço do mundo, onde o espaço do mundo é 
+            //definido como o sistema de coordenadas no topo da hierarquia do seu jogo.
+            // pega a posição do mouse que está na tela ( visão da camêra )
+            Vector3 pontoZ = new Vector3(ponto.x, ponto.y, objeto.transform.position.z);
+            // a camêra está em (0, 0, -10), isso é pra que o objeto apareça
+            Spanw(pontoZ);
+
+
+        }
+    }
+}
