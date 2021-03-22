a062bdc5c3e7cbdbdac526b7c4d0502993a2e550
Filipe Lima <leonardodiasbicalho@gmail.com>
2021-03-21 22:02:16 -0300

    Commit atualization

---
+++ BubbleProject/Assets/Scripts/atualizadorDeOnda.cs
@@ -0,0 +1,23 @@
+﻿/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class atualizadorDeOnda : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject [] listaOndas;
+    private int tamanho;
+
+    void Start()
+    {
+        listaOndas = new List<GameObject>();*/
+   /* }*/
+/*
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+*/
