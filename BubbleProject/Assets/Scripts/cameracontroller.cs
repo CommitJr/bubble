bcec9788c097b38b6bd8950f6fa09e74fd4d7d38
Filipe Lima <leonardodiasbicalho@gmail.com>
2021-03-24 16:08:52 -0300

    controle de camera e novos assets

---
+++ BubbleProject/Assets/Scripts/cameracontroller.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameracontroller : MonoBehaviour
+{
+
+    public GameObject personagem;
+    public GameObject cameraPositionAtual;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 cameraNovaPosition = cameraPositionAtual.transform.position;
+
+        cameraNovaPosition.y = personagem.transform.position.y;
+
+        cameraPositionAtual.transform.position = cameraNovaPosition;
+
+    }
+}
