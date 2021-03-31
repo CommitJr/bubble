869c057766f0f5bdbcb677dbebb8aa92ffc6be9e
Marina <aniram.520@gmail.com>
2021-03-31 18:32:30 -0300

    alterações

--- BubbleProject/Assets/Scripts/colisaoBolha.cs
+++ BubbleProject/Assets/Scripts/colisaoBolha.cs
@@ -8,20 +8,21 @@ public class colisaoBolha : MonoBehaviour
     // Start is called before the first frame update
 
 
-    [SerializeField] private Collider2D colisorMaior;
 
     void Start(){
-        colisorMaior = GetComponent<Collider2D>();
+        
     }
 
     void Update(){
+
+
     }
 
-    /*
-
-    void OnTriggerEnter2D(Collider2D obstaculo){
-        if(obstaculo.gameObject.CompareTag("inimigo"))
-            Debug.Log("bolha estourou");
-
-    }*/
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == 9)
+        {
+            print("bolha estourou");
+        }
+    }
 }
---
+++ BubbleProject/Assets/Scripts/movimento.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movimento : MonoBehaviour
+{
+    [Header("Movimento")]
+    public float velocidade;
+    private int direcao = -1;
+    [Header("Componentes")]
+    public Rigidbody2D inimigoRb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+    private void FixedUpdate()
+    {
+        Move();
+    }
+    private void Move()
+    {
+        inimigoRb.velocity = new Vector2(velocidade * direcao, inimigoRb.velocity.y);
+        print("MOVE PRAGA");
+    }
+    void OnCollisionEnter2D(Collision2D parede)
+    {
+        if (parede.transform.tag == "parede")
+        {
+            direcao *= -1;
+            transform.eulerAngles = new Vector2(0f, 180);
+            if(direcao == -1)
+            {
+                transform.eulerAngles = new Vector2(0f, 0);
+            }
+            print(direcao);
+        }
+    }
+}
