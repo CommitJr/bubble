213f705cd5a0c1272f1c16c822fc6328fc085ff6
Marina <aniram.520@gmail.com>
2021-03-30 22:34:08 -0300

    alterações

--- BubbleProject/Assets/Scripts/onda.cs
+++ BubbleProject/Assets/Scripts/onda.cs
@@ -7,29 +7,34 @@ using UnityEngine;
 
 public class onda : MonoBehaviour
 {
-
+    private Collider2D colisor;
     public GameObject objeto;
-    // Start is called before the first frame update
-    public void Spanw(Vector3 position)
+    void Start()
     {
-        Instantiate(objeto).transform.position = position;
-        // cria o objeto que faz a 'onda'
+        colisor = GetComponent<Collider2D>();
     }
 
+  
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
             Vector3 ponto = Camera.main.ScreenToWorldPoint(Input.mousePosition, Camera.MonoOrStereoscopicEye.Mono);
-            // segundo a Unity doc : Transforma um ponto do espaço da tela em espaço do mundo, onde o espaço do mundo é 
-            //definido como o sistema de coordenadas no topo da hierarquia do seu jogo.
-            // pega a posição do mouse que está na tela ( visão da camêra )
             Vector3 pontoZ = new Vector3(ponto.x, ponto.y, objeto.transform.position.z);
-            // a camêra está em (0, 0, -10), isso é pra que o objeto apareça
-            Spanw(pontoZ);
+            transform.position = pontoZ;
 
-
+            colisor.enabled = true;
+           
+            print("colisor");
+        }
+        if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+           
+            colisor.enabled = false;
+            print(" não colisor");
         }
     }
+
+
 }
---
+++ BubbleProject/Assets/Scripts/teste.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class teste : MonoBehaviour
+{
+    private Vector3 ponto;
+    private Collider2D colisor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        colisor = GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            colisor.enabled = !colisor.enabled;
+        
+        }
+      
+    }
+}
