d5be252f62108f79c5c0956ba7d859f75f622662
Filipe Lima <leonardodiasbicalho@gmail.com>
2021-04-06 02:00:37 -0300

    isso conserta a bolha
    ainda tenho que transformar algumas coisas em variaveis e implementar o click not pressed

--- BubbleProject/Assets/Scripts/bolhaController.cs
+++ BubbleProject/Assets/Scripts/bolhaController.cs
@@ -11,11 +11,27 @@ public class bolhaController : MonoBehaviour
     private Vector3 swipeDirection;
     [SerializeField] private int velocidade = 100;
     [SerializeField] private Collider2D colisorMouse;
-
+    [SerializeField] private float velocity;
     
-    void Update(){
+    void FixedUpdate(){
         // swipeDirection = new Vector3(colisorMouse.transform.position.y, colisorMouse.transform.position.x, colisorMouse.transform.position.z);
         //swipeDirection.Normalize();
+        if (GetComponent<Rigidbody2D>().velocity.x > velocity)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(velocity, GetComponent<Rigidbody2D>().velocity.y);
+        }
+        if (GetComponent<Rigidbody2D>().velocity.y > velocity)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x,velocity);
+        }
+        if (GetComponent<Rigidbody2D>().velocity.x < -velocity)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-velocity, GetComponent<Rigidbody2D>().velocity.y);
+        }
+        if (GetComponent<Rigidbody2D>().velocity.y < -velocity)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, -velocity);
+        }
     }
 
     void OnTriggerEnter2D(Collider2D objetoQualquer){
