5daca64483d1340bf6a436f27783ff7ca9a46bbb
Filipe Lima <leonardodiasbicalho@gmail.com>
2021-04-07 13:22:21 -0300

    melhorando codigo bolha e ajustes finos

--- BubbleProject/Assets/Scripts/bolhaController.cs
+++ BubbleProject/Assets/Scripts/bolhaController.cs
@@ -9,28 +9,24 @@ using UnityEngine;
 public class bolhaController : MonoBehaviour
 {
     private Vector3 swipeDirection;
-    [SerializeField] private int velocidade = 100;
+    [SerializeField] private int forca = 120;
     [SerializeField] private Collider2D colisorMouse;
-    [SerializeField] private float velocity;
+    [SerializeField] private float velocidadeLimite = 2.5f;
     
     void FixedUpdate(){
         // swipeDirection = new Vector3(colisorMouse.transform.position.y, colisorMouse.transform.position.x, colisorMouse.transform.position.z);
         //swipeDirection.Normalize();
-        if (GetComponent<Rigidbody2D>().velocity.x > velocity)
-        {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(velocity, GetComponent<Rigidbody2D>().velocity.y);
+        if (GetComponent<Rigidbody2D>().velocity.x > velocidadeLimite){
+            GetComponent<Rigidbody2D>().velocity = new Vector2(velocidadeLimite, GetComponent<Rigidbody2D>().velocity.y);
         }
-        if (GetComponent<Rigidbody2D>().velocity.y > velocity)
-        {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x,velocity);
+        if (GetComponent<Rigidbody2D>().velocity.y > velocidadeLimite){
+            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, velocidadeLimite);
         }
-        if (GetComponent<Rigidbody2D>().velocity.x < -velocity)
-        {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(-velocity, GetComponent<Rigidbody2D>().velocity.y);
+        if (GetComponent<Rigidbody2D>().velocity.x < -velocidadeLimite){
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-velocidadeLimite, GetComponent<Rigidbody2D>().velocity.y);
         }
-        if (GetComponent<Rigidbody2D>().velocity.y < -velocity)
-        {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, -velocity);
+        if (GetComponent<Rigidbody2D>().velocity.y < -velocidadeLimite){
+            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, -velocidadeLimite);
         }
     }
 
@@ -39,12 +35,9 @@ public class bolhaController : MonoBehaviour
 
             Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-            //GetComponent<Rigidbody2D>().AddForce(-objetoQualquer.GetComponent<Rigidbody>().velocity.normalized * velocidade);
-            //GetComponent<Rigidbody2D>().AddForce(-transform.TransformDirection(colisorMouse.transform.position).normalized * velocidade);
-            
             Vector2 direction = new Vector2(transform.position.x - mousePosition.x, transform.position.y - mousePosition.y).normalized;
 
-            GetComponent<Rigidbody2D>().AddForce(direction * velocidade);
+            GetComponent<Rigidbody2D>().AddForce(direction * forca);
         }
     }
 }
