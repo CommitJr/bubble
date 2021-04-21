f5562ed0e52b13cb38128d29d04c7aabd2d0e489
Filipe Lima <leonardodiasbicalho@gmail.com>
2021-04-21 14:50:48 -0300

    Correções

--- BubbleProject/Assets/Scripts/matouBolha.cs
+++ BubbleProject/Assets/Scripts/matouBolha.cs
@@ -1,16 +1,13 @@
-﻿
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class colisaoBolha : MonoBehaviour
+public class matouBolha : MonoBehaviour
 {
     // Start is called before the first frame update
-
     
     public GameObject tcena;
     trocacena scripttcena;
-
 
     void Start(){
         scripttcena = tcena.GetComponent<trocacena>();       
@@ -18,11 +15,13 @@ public class colisaoBolha : MonoBehaviour
 
     public void OnCollisionEnter2D(Collision2D collision)
     {   
-        if(collision.gameObject.tag == "end"){
+
+        if (collision.gameObject.tag == "Player") {
 
             scripttcena.IniciaTransicao(0);
             scripttcena.MudaCena();
 
         }
+
     }
 }
