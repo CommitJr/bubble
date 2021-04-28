182368e1afb80f44d730e819ad5e895d15730b42
Marina <aniram.520@gmail.com>
2021-04-28 19:47:14 -0300

    alterações

---
+++ BubbleProject/Assets/Scripts/pauseSystem.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class pauseSystem : MonoBehaviour
+{
+    public static bool pausa = false;
+    public GameObject pausaMenuUI;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+           
+            if (pausa)
+            {
+                Resumo();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+    public void Resumo()
+    {
+        pausaMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        pausa = false;
+    }
+    void Pause()
+    {
+        pausaMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        pausa = true;
+    }
+    public void MenuButton()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+    public void SairButton()
+    {
+        Debug.Log("Saindo do jogo...");
+        Application.Quit();
+    }
+}
---
+++ BubbleProject/Assets/menuSystem.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class menuSystem : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    //    SceneManager.LoadScene(1);
+
+    }
+    public void Sair()
+    {
+        Debug.Log("Saindo do jogo...");
+        Application.Quit();
+    }
+}
