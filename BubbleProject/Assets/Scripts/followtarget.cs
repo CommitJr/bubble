e46d8ed703bf5873c7446fbe6fc864a4e65daad1
matheustheus27 <31577877+matheustheus27@users.noreply.github.com>
2021-05-14 18:19:23 -0300

    Peixe Dumbo
    Mecânica Principal do Peixe Dumbo

---
+++ BubbleProject/Assets/Scripts/followtarget.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followtarget : MonoBehaviour
+{
+    private Vector2 direction;
+    private GameObject player;
+    [SerializeField] private float velocity;
+    private float angle;
+    private Rigidbody2D rg2D;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+        rg2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        direction = (player.transform.position - transform.position);
+        Debug.Log(direction.magnitude);
+        if (direction.magnitude < 10)
+        {
+            direction = direction.normalized;
+            direction *= velocity;
+            angle = Vector2.SignedAngle(Vector2.up, direction);
+            transform.rotation = Quaternion.Euler(0, 0, angle);
+            rg2D.velocity = direction;
+        }
+        else
+        {
+            rg2D.velocity = Vector2.zero;
+        } 
+    }
+
+}
