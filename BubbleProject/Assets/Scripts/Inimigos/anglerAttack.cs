95947937a7b30436c8e087f56aad563fcff4b495
Matheus Thiago <31577877+matheustheus27@users.noreply.github.com>
2021-06-21 22:30:24 -0300

    Que haja luz! E a luz começou a existir
    Mecânica do Angler criada/adicionada
    Se mantem na escuridão e pisca sua luz com intensidade ao detectar a aproximação do player

---
+++ BubbleProject/Assets/Scripts/Inimigos/anglerAttack.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.LWRP;
+
+public class anglerAttack : MonoBehaviour
+{
+    public UnityEngine.Experimental.Rendering.Universal.Light2D intensity;
+    private Vector2 direction;
+    private GameObject player;
+    private Contador contador;
+    private bool ativado;
+    // Start is called before the first frame update
+    void Start()
+    {
+        intensity = GetComponent<UnityEngine.Experimental.Rendering.Universal.Light2D>();
+        player = GameObject.FindWithTag("Player");
+        contador = new Contador(.25f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        direction = (player.transform.position - transform.position);
+        if (direction.magnitude < 6) {
+            lightAttack();
+        }
+        else
+        {
+            intensity.intensity = 0.8f;
+            intensity.pointLightOuterRadius = 0.5f;
+        }
+       
+    }
+
+    void lightAttack() {
+        if (contador.RepeatCountTime())
+        {
+
+            if (ativado)
+            {
+                intensity.intensity = 10;
+                intensity.pointLightOuterRadius = 10;
+                ativado = false;
+            }
+            else 
+            {
+                intensity.intensity = 1;
+                ativado = true;
+            }
+
+        }
+
+        
+        
+    }
+}
