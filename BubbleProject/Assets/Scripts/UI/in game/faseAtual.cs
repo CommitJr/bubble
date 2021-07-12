6c01831d8008a138b5fe476d863421610763ddce
Marina <aniram.520@gmail.com>
2021-07-12 19:53:05 -0300

    interface de pausa quase finalizada

---
+++ BubbleProject/Assets/Scripts/UI/in game/faseAtual.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class faseAtual : MonoBehaviour
+{
+    private TextMeshProUGUI text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<TextMeshProUGUI>(); 
+        text.SetText(SceneManager.GetActiveScene().name);
+    }
+
+
+}
