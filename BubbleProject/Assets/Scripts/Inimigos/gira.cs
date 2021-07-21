7545da9cbe3adc37e76395d802c08de4fa74be70
Filipe Lima <leonardodiasbicalho@gmail.com>
2021-07-21 03:53:06 -0300

    lula gigante, com bugs
    lula gigante, faltando attack a ser implementado e apresentando deficiências na movimentação. Pode ser preciso repensar seu tamanho e area de atuação. Ainda não mata a bolha

---
+++ BubbleProject/Assets/Scripts/Inimigos/gira.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gira : MonoBehaviour
+{
+    [Header("Movimento")]
+    [SerializeField] private float angulacao = 1;
+    [Header("Componentes")]
+    [SerializeField] private Transform inimigoT;
+    
+
+    void OnCollisionEnter2D(Collision2D parede)
+    {
+        if (parede.transform.tag == "parede" || parede.transform.tag == "Pedras")
+        {
+            angulacao *= -1;
+            Quaternion target = Quaternion.Euler(0, 0, angulacao);
+            inimigoT.rotation = Quaternion.Slerp(inimigoT.rotation, target,  Time.deltaTime * 2);
+        }
+    }
+}
