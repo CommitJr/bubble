b0f26ae97710e3e3f0d4679430f2675c832b8753
Filipe Lima <leonardodiasbicalho@gmail.com>
2021-07-26 18:16:03 -0300

    Lula Ta batendo

---
+++ BubbleProject/Assets/Scripts/Inimigos/lulaAtack.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class lulaAtack : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    [SerializeField] private Animator animator;
+    private float distancia;
+
+    
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        player = GameObject.FindWithTag("Player");
+    }
+
+
+    void FixedUpdate()
+    {
+        distancia = Vector3.Distance(player.transform.position, transform.position); 
+        animator.SetFloat("distancia", distancia);
+    }
+
+}
