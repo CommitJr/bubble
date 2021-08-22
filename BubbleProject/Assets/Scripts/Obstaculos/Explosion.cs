09959c1124c851f07af2f23c1e4b6ff9fd7b702f
Marina <aniram.520@gmail.com>
2021-08-22 16:21:34 -0300

    Explosion

---
+++ BubbleProject/Assets/Scripts/Obstaculos/Explosion.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    public float delay = 1f;
+    public GameObject exp;
+    float countdown;
+    bool hasExploded = false;
+    public float radius = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        countdown = delay;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void Explode()
+    {
+        Instantiate(exp, transform.position, transform.rotation);
+
+        Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
+
+        foreach (Collider nearbyObject in colliders)
+        {
+            Rigidbody rb = nearbyObject.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.AddExplosionForce(700f, transform.position, radius);
+            }
+        }
+        Destroy(gameObject);
+    }
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            countdown -= Time.deltaTime;
+            if (countdown <= 0f && !hasExploded)
+            {
+                Explode();
+                hasExploded = true;
+            }
+        }
+    }
+}
+
