4d3edd78f31afd066cb9fa9c51c12a9f08d619dc
Marina <aniram.520@gmail.com>
2021-08-22 11:01:57 -0300

    correção de bugs
    -pedras aparecem na tela

---
+++ BubbleProject/Assets/Scripts/Inimigos/Falling.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Falling : MonoBehaviour
+{
+    private GameObject player;
+    private Rigidbody2D rb;
+    private Vector2 direction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+        
+        rb = GetComponent<Rigidbody2D>();
+        rb.gravityScale = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+          direction = (player.transform.position - transform.position);
+          if (direction.magnitude < 20)
+          {
+                rb.gravityScale = 1f;
+        }
+    }
+}
