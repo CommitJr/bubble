f5517aec793a29cc207b4e76a1598b7f7577f869
Matheus Thiago <31577877+matheustheus27@users.noreply.github.com>
2021-08-23 01:06:02 -0300

    Vivendo e Aprendendo
    - Tutorial parcialmente implementado

---
+++ BubbleProject/Assets/Scripts/Geral/backgroundLoop.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class backgroundLoop : MonoBehaviour
+{
+    [SerializeField] private MeshRenderer mesh;
+    [SerializeField] private GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        mesh.material.mainTextureOffset = new Vector2(player.transform.position.x/20, player.transform.position.y/20);
+    }
+}
---
+++ BubbleProject/Assets/Scripts/UI/Tutorial/desativaCanvas.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class desativaCanvas : MonoBehaviour
+{
+    [SerializeField] private GameObject desativa;
+    [SerializeField] private GameObject ativa;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Action() {
+        desativa.SetActive(false);
+        ativa.SetActive(true);
+    }
+}
