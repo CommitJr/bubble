0d9c1f552afaaeb74bddca48a32d601f4cfcf454
Matheus Thiago <31577877+matheustheus27@users.noreply.github.com>
2021-08-24 02:25:59 -0300

    Ajudinha de Leve
    - Tutorial está 90% pronto
    - Os textos precisam ser melhorados
    - Alguns objetos precisam ser melhorados

---
+++ BubbleProject/Assets/Scripts/UI/Tutorial/moveParede.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveParede : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    [SerializeField] private GameObject[] tutorial;
+    private int contTutorial = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (contTutorial < 2)
+        {
+            gameObject.transform.position = new Vector2(gameObject.transform.position.x, player.transform.position.y);
+        }
+        else 
+        {
+            gameObject.transform.position = new Vector2(player.transform.position.x, gameObject.transform.position.y);
+        }     
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+            tutorial[contTutorial].SetActive(false);
+            tutorial[contTutorial + 1].SetActive(true);
+            contTutorial++;
+
+            switch (contTutorial)
+            {
+                case 1:
+                gameObject.transform.position = new Vector2(gameObject.transform.position.x - 10, gameObject.transform.position.y);
+                    break;
+                case 2:
+                    gameObject.transform.position = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y - 10);
+                    gameObject.transform.Rotate(new Vector3(0, 0, 90));
+                    break;
+            case 3:
+                gameObject.transform.position = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y + 10);
+                break;
+            case 4:
+                gameObject.SetActive(false);
+                break;
+
+        }
+
+        print(contTutorial);
+  
+    }
+}
