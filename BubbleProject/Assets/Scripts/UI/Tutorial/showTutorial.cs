355789dacb81bdf2c283f8d59f733bfdebf9d537
Marina <aniram.520@gmail.com>
2021-09-05 22:39:20 -0300

    primeira build
    -- ui de mostrar tutorial muito pequena
    -- gota da camera

---
+++ BubbleProject/Assets/Scripts/UI/Tutorial/showTutorial.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class showTutorial : MonoBehaviour
+{
+    [SerializeField] private GameObject TutorialUI;
+    public void show()
+    {
+        playerData data = saveSystem.LoadPlayer();
+        if (data == null)
+        {
+            TutorialUI.SetActive(true);
+        }
+        else
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+        }
+    }
+}
