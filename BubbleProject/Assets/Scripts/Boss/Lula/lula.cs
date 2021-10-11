15ad7e16bcb76037832023eec2f0b86dc4838b95
Marina <aniram.520@gmail.com>
2021-10-11 10:02:14 -0300

    Ataque da Lula

---
+++ BubbleProject/Assets/Scripts/Boss/Lula/lula.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lula : MonoBehaviour
+{
+    public GameObject oleo;
+    [SerializeField] private float speed = 300;
+    private Transform player;
+    private float dirX, dirY;
+
+    [SerializeField] private float rate;
+    private float next;
+
+    void Start()
+    {
+        rate = 1f;
+        next = Time.time;
+    }
+    void Update()
+    {
+        Fogo();
+    }
+
+    void Fogo()
+    {
+        if (Time.time > next)
+        {
+            Debug.Log(transform.position);
+            Instantiate(oleo, transform.position, Quaternion.identity);
+            next = Time.time + rate;
+        }
+    }
+}
---
+++ BubbleProject/Assets/Scripts/Boss/Lula/projetil.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class projetil : MonoBehaviour
+{
+    
+    [SerializeField] private float speed = 7f;
+    Rigidbody2D rb;
+    Vector2 diretion;
+
+    [SerializeField] private GameObject player;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("BolhaRastreio");
+        rb = GetComponent<Rigidbody2D>();
+
+        diretion = (player.transform.position - transform.position).normalized * speed;
+        rb.velocity = new Vector2(diretion.x, diretion.y);
+
+        Debug.Log(diretion);
+
+        Destroy(gameObject, 3f);
+                
+    }
+
+    
+}
