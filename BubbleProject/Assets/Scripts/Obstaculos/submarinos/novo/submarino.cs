bd940edaf68798045b0bf73ece281e556e1b6c58
Marina <aniram.520@gmail.com>
2021-10-11 14:20:01 -0300

    fase 3 de 3
    - falta a mecânica da lula vampiro

---
+++ BubbleProject/Assets/Scripts/Obstaculos/submarinos/novo/submarino.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class submarino : MonoBehaviour
+{
+    public GameObject torpedo;
+    [SerializeField] private float speed = 300;
+    private Transform player;
+    private float dirX, dirY;
+
+    [SerializeField] private float rate;
+    private float next;
+
+    void Start()
+    {
+        next = Time.time;
+    }
+    void Update()
+    {
+        Fogo();
+    }
+
+    void Fogo()
+    {
+        if (Time.time > next)
+        {
+            Debug.Log(transform.position);
+            Instantiate(torpedo, transform.position, Quaternion.identity);
+            next = Time.time + rate;
+        }
+    }
+}
---
+++ BubbleProject/Assets/Scripts/Obstaculos/submarinos/novo/torpedo.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class torpedo : MonoBehaviour
+{
+    [SerializeField] private float speed = 7f;
+    Rigidbody2D rb;
+    Vector2 diretion;
+
+    [SerializeField] private GameObject alvo;
+ 
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        
+        Invoke("liberacao", 2f);
+
+        Destroy(gameObject, 10f);
+    }
+
+
+
+    void liberacao()
+    {
+        rb.gravityScale = 0f;
+        diretion = (alvo.transform.position - transform.position).normalized * speed;
+        rb.velocity = new Vector2(diretion.x, diretion.y);
+    }
+}
