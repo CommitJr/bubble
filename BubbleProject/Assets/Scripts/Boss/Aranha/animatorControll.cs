0dd6540820cfd5efa758b64479c3e8ab49224393
Marina <aniram.520@gmail.com>
2021-10-11 11:12:55 -0300

    construindo fases
    - camada 3: fase 1

--- BubbleProject/Assets/Scripts/Boss/Aranha/animatorControll.cs
+++ BubbleProject/Assets/Scripts/Boss/Aranha/animatorControll.cs
@@ -2,8 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-// ataque da garra esquerda
-public class ataqueE : MonoBehaviour
+public class animatorControll : MonoBehaviour
 {
     private Transform player;
     private Rigidbody2D rg2D;
@@ -22,12 +21,35 @@ public class ataqueE : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // falta encontrar valores melhores para a area de ataque( direita, esquerda e centro )
+
 
         direction = (player.position - centro.position);
 
+        // ataque esquerdo
         if (Vector2.Distance(centro.position, player.position) < 3)
         {
             animator.SetBool("ataque_e", true);
+
+            direction = direction.normalized;
+            direction *= velocity;
+            rg2D.velocity = direction;
+        }
+
+        // ataque direito
+        if (Vector2.Distance(centro.position, player.position) < 3)
+        {
+            animator.SetBool("ataque_d", true);
+
+            direction = direction.normalized;
+            direction *= velocity;
+            rg2D.velocity = direction;
+        }
+
+        // ataque central
+        if (Vector2.Distance(centro.position, player.position) < 3)
+        {
+            animator.SetBool("abraco", true);
 
             direction = direction.normalized;
             direction *= velocity;
@@ -38,6 +60,8 @@ public class ataqueE : MonoBehaviour
         {
             rg2D.velocity = Vector2.zero;
             animator.SetBool("ataque_e", false);
+            animator.SetBool("abraco", false);
+            animator.SetBool("ataque_d", false);
         }
     }
 }
---
+++ BubbleProject/Assets/Scripts/Inimigos/Ataques/peixe dragão/dragonFire.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dragonFire : MonoBehaviour
+{
+    private ParticleSystem part;
+    private Vector2 direction;
+    private Transform player;
+    [SerializeField] private Transform centro;
+
+
+    void Start()
+    {
+        ParticleSystem part = GetComponent<ParticleSystem>();
+        player = GameObject.FindGameObjectWithTag("BolhaRastreio").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        direction = (player.position - centro.position);
+
+        if (Vector2.Distance(centro.position, player.position) < 10)
+        {
+            part.Play();
+        }
+        else
+        {
+            part.Stop();
+        }
+    }
+}
