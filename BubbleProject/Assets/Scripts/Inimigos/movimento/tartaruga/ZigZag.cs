f92085eb844dbbaec2ffc18a4bb86121d2b27787
Marina <aniram.520@gmail.com>
2021-11-30 17:23:13 -0300

    movimento tartaruga
    - zigzagiando

---
+++ BubbleProject/Assets/Scripts/Inimigos/movimento/tartaruga/ZigZag.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZigZag : MonoBehaviour
+{
+    [SerializeField] private Transform[] waypoints;
+    [SerializeField] private float moveSpeed = 2f;
+
+    private int waypointIndex = 0;
+    private bool canGoBack = false;
+    private void Start()
+    {
+        transform.position = waypoints[waypointIndex].transform.position;
+    }
+
+    private void Update()
+    {
+        Move();
+        Wayback();
+        Debug.Log(canGoBack);
+    }
+    private void Move()
+    {
+        if (waypointIndex <= waypoints.Length - 1 && !canGoBack)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, waypoints[waypointIndex].transform.position, moveSpeed * Time.deltaTime);
+
+            if (transform.position == waypoints[waypointIndex].transform.position)
+            {
+                waypointIndex += 1;
+            }
+            if (waypointIndex > waypoints.Length - 1)
+            {
+                canGoBack = true;
+            }
+        }
+    }
+
+    private void Wayback()
+    {
+        if (canGoBack)
+        {
+            if (waypointIndex <= waypoints.Length || waypointIndex >= waypoints.Length - 2)
+             {
+               transform.position = Vector2.MoveTowards(transform.position, waypoints[waypointIndex-1].transform.position, moveSpeed * Time.deltaTime);
+               if (transform.position == waypoints[waypointIndex-1].transform.position)
+               {
+                   waypointIndex -= 1;
+               }
+
+               if (waypointIndex < waypoints.Length - 2 )
+               {
+                   canGoBack = false;
+               }
+
+            }
+        }
+        
+    }
+}
