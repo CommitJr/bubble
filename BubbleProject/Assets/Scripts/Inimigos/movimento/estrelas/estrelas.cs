a604e0685f54cba8b23bf5b1682ddf53f6148fcd
Marina <aniram.520@gmail.com>
2021-12-02 15:15:36 -0300

    estrelas

--- BubbleProject/Assets/Scripts/Inimigos/movimento/estrelas/estrelas.cs
+++ BubbleProject/Assets/Scripts/Inimigos/movimento/estrelas/estrelas.cs
@@ -4,23 +4,20 @@ using UnityEngine;
 
 public class estrelas : MonoBehaviour
 {
-    private Vector2 direction;
     private Transform player;
     [SerializeField] private float velocity;
-    private float angle;
+    private Contador contador;
     private Rigidbody2D rg2D;
-
-    [SerializeField] private float rate;
-    private float next;
-
+    private float rate;
+    [SerializeField] private GameObject estrelaPrefab;
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("BolhaRastreio").GetComponent<Transform>();
         rg2D = GetComponent<Rigidbody2D>();
         rg2D.gravityScale = 0f;
-
         rate = 5f;
-        next = Time.time;
+        contador = new Contador(rate);
+        Debug.Log(transform.parent.transform.position);
     }
 
     // Update is called once per frame
@@ -29,25 +26,23 @@ public class estrelas : MonoBehaviour
         if (Vector2.Distance(transform.position, player.position) < 50)
         {
             rg2D.gravityScale = 1f;
-            
+           
+         //   Destroy(gameObject, 7f);
         }
         else
         {
             rg2D.gravityScale = 0f;
-        }
-        
-        /*
+        } 
         Multiplicar();
-        Destroy(gameObject, 5f);
-        */   
     }
 
     void Multiplicar()
     {
-        if (Time.time > next)
+        if (contador.RepeatCountTime())
         {
-            Instantiate(this);
-            next = Time.time + rate;
+            Debug.Log(transform.parent.transform.position);
+            GameObject star = Instantiate(estrelaPrefab, transform.parent);
+            star.transform.position = transform.parent.position;
         }
 
         
