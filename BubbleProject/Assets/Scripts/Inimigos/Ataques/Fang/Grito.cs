554591dca49f6e5d36b2f9e09996a842280ceb8d
Marina <aniram.520@gmail.com>
2021-12-08 11:18:37 -0300

    ajustes camada 4

---
+++ BubbleProject/Assets/Scripts/Inimigos/Ataques/Fang/Grito.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grito : MonoBehaviour
+{
+   [SerializeField] private GameObject ondaPrefab;
+    [SerializeField] private Transform spawnPosition;
+    private Contador contador;
+
+    [SerializeField] private int temporizador;
+    // Start is called before the first frame update
+    void Start()
+    {
+        contador = new Contador(3);
+        contador = new Contador(temporizador);
+    }
+
+    // Update is called once per frame
+    void Update(){
+    
+        if(contador.RepeatCountTime()){
+            CriaOnda();
+        }
+    }
+    private void CriaOnda(){
+        Instantiate(ondaPrefab, spawnPosition.position, Quaternion.identity);
+    }
+
+}
