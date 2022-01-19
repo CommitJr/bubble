554bcc69fdd0b80d91815ade928982456fb5415f
Marina <aniram.520@gmail.com>
2022-01-19 16:10:49 -0300

    FASE 1 FINALMENTE PRONTA

---
+++ BubbleProject/Assets/Scripts/Inimigos/Ataques/Tubarao/WaitPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaitPlayer : MonoBehaviour
+{
+    private Transform player;
+    [SerializeField] private Transform centro;
+    private viboraAttack attackScript;
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("BolhaRastreio").GetComponent<Transform>();
+        attackScript = gameObject.GetComponent<viboraAttack>();
+    }
+    void Update()
+    {
+        if (Vector2.Distance(centro.position, player.position) < 5)
+        {
+            attackScript.enabled = true;
+        }
+        else
+        {
+            attackScript.enabled = false;
+        }
+    }
+}
