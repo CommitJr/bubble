44df679253d625cb37580449a6e68dc8b147c8fe
Marina <aniram.520@gmail.com>
2022-02-14 12:20:51 -0300

    Melhora no polvo
    - ele agarra a bolha

--- BubbleProject/Assets/Scripts/Inimigos/Ataques/Polvo/AnimatorControl.cs
+++ BubbleProject/Assets/Scripts/Inimigos/Ataques/Polvo/AnimatorControl.cs
@@ -6,25 +6,32 @@ public class AnimatorControl : MonoBehaviour
 {
     [SerializeField] private Animator animatorControl;
     private Transform player;
-
+    bool _canAttack;
     [SerializeField] private Transform centro;
     private Vector2 direction;
+    private PlayerController playerController;
+
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("BolhaRastreio").GetComponent<Transform>();
         if (!animatorControl) animatorControl = GetComponent<Animator>();
+        _canAttack = true;
+        playerController = GameObject.FindGameObjectWithTag("PlayerController").GetComponent<PlayerController>();
     }
 
     void Update()
     {
-        if (Vector2.Distance(centro.position, player.position) < 5)
+        if (_canAttack)
         {
-            Debug.Log("bolha na area");
-            animatorControl.SetBool("_toAttack", true);
-            Debug.Log("preparando para o ataque");
-            StartCoroutine(Change(animatorControl.GetCurrentAnimatorStateInfo(0).length));
-            Debug.Log("realizando o ataque");
-            animatorControl.SetBool("_canAttack", true);
+            if (Vector2.Distance(centro.position, player.position) < 5)
+            {
+             //   Debug.Log("bolha na area");
+                animatorControl.SetBool("_toAttack", true);
+           //     Debug.Log("preparando para o ataque");
+                StartCoroutine(Change(animatorControl.GetCurrentAnimatorStateInfo(0).length));
+             //   Debug.Log("realizando o ataque");
+                animatorControl.SetBool("_canAttack", true);
+            }
         }
     }
 
@@ -32,4 +39,15 @@ public class AnimatorControl : MonoBehaviour
     {
         yield return new WaitForSeconds(_delay);
     }
+
+    public void StopAttack()
+    {
+        _canAttack = false;
+        Debug.Log(_canAttack);
+        animatorControl.SetBool("_toAttack", false);
+        animatorControl.SetBool("_canAttack", false);
+        playerController.KillPlayer();
+    }
+    
+  
 }
---
+++ BubbleProject/Assets/Scripts/Inimigos/Ataques/Polvo/ArmAttack.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmAttack : MonoBehaviour
+{
+    [SerializeField] private AnimatorControl animator;
+    private PlayerController onda;
+
+    private void Start()
+    {
+        onda = GameObject.FindGameObjectWithTag("PlayerController").GetComponent<PlayerController>();
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision) 
+    {
+        if (collision.tag == "BolhaRastreio")
+        {
+            Debug.Log(collision.tag);
+            onda.SetControllerActivate(false);
+            //    collision.transform.parent.GetComponent<BubbleController>().enabled = false;
+            collision.transform.parent.GetComponent<Rigidbody2D>().simulated = false;
+            collision.transform.parent.SetParent(transform);
+            collision.transform.localPosition = Vector2.zero;
+            animator.StopAttack();
+        }
+    }
+}
