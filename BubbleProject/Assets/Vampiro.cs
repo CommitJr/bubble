a191823aaeee33dee886a11f5b89700c8285e1b6
Matheus Thiago <matheustheus27@gmail.com>
2022-08-30 23:25:39 -0300

    Sugando até a alma
    - Lula Vampiro funcionando
    - SDK Target: 33

--- BubbleProject/Assets/Vampiro.cs
+++ BubbleProject/Assets/Vampiro.cs
@@ -14,8 +14,10 @@ public class Vampiro : MonoBehaviour
     [SerializeField] float maxDistance;
     private PlayerController playerController;
     private Vector2 direction;
+    private Contador contador;
     [SerializeField] private Transform center;
     [SerializeField] private Transform lingua;
+    [SerializeField] private float attackTime;
 
     void Start()
     {
@@ -27,8 +29,8 @@ public class Vampiro : MonoBehaviour
         canAttack = false;
         lineRenderer = GetComponent<LineRenderer>();
         lineRenderer.useWorldSpace = true;
+        contador = new Contador(attackTime);
 
-       
         direction = new Vector2(player.position.x - center.position.x, player.position.y - center.position.y).normalized;
         playerController = GameObject.FindGameObjectWithTag("PlayerController").GetComponent<PlayerController>();
     }
@@ -57,31 +59,24 @@ public class Vampiro : MonoBehaviour
                     }
              }
          
-             if (canAttack)
-             {
+            if (canAttack)
+            {
                 Vector3 desiredPosition = direction * 10;
-                if (Vector2.Distance(pointPosition, center.position + desiredPosition) > 1)
-                {
-                    
-                    pointPosition = Vector3.Lerp(pointPosition, center.position + desiredPosition, time );
-                    lineRenderer.SetPosition(0, center.position);
-                    lineRenderer.SetPosition(1, pointPosition);
-                }
-                else
+                pointPosition = Vector3.Lerp(pointPosition, center.position + desiredPosition, time );
+                lineRenderer.SetPosition(0, center.position);
+                lineRenderer.SetPosition(1, pointPosition);
+
+                if (contador.RepeatCountTime())
                 {
                     canAttack = false;
-                }
-                        
-             }
-            else
+                }   
+            }else
             {
-                if (Vector2.Distance(pointPosition, center.position) > 1)
-                {
-                    pointPosition = Vector3.Lerp(pointPosition, center.position, time);
-                    lineRenderer.SetPosition(0, center.position);
-                    lineRenderer.SetPosition(1, pointPosition);
-                }
-                else
+                pointPosition = Vector3.Lerp(pointPosition, center.position, time);
+                lineRenderer.SetPosition(0, center.position);
+                lineRenderer.SetPosition(1, pointPosition);
+
+                if (contador.RepeatCountTime())
                 {
                     canAttack = true;
                 }
